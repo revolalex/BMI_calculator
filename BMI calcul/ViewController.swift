70d0a1092b81524a2100bf296ae85761f5b41314
Alex <revolalex@gmail.com>
2019-12-06 20:00:48 +0100

    Initial Commit

---
+++ BMI calcul/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI calcul
+//
+//  Created by alexandre rodriguez on 06/12/2019.
+//  Copyright © 2019 alexandre rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
