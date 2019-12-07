ae001a2e0f8e453be0ab15e603b7d1bed9d8c323
Alex <revolalex@gmail.com>
2019-12-07 12:18:08 +0100

    first commit

--- BMI calcul/ViewController.swift
+++ BMI calcul/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func weightValueSet(_ sender: UISlider) {
+        updateValues()
+    }
+    
+    @IBAction func heightValueSet(_ sender: UISlider) {
+        updateValues()
+    }
+    func updateValues() {
+        let weight = weightSlider.value
+        let height = heightSlider.value
+        let bmi = weight / (height * height)
+        weightLabel.text = "\(formatNumber(value: weight)) Kg"
+        heightLabel.text = "\(formatNumber(value: height)) M"
+        bmiLabel.text = "BMI = \(bmi)"
+        
+        
+    switch bmi {
+    case 0...18.5:
+        categoryLabel.backgroundColor = UIColor.lightGray
+        categoryLabel.text = "Underweight"
+    case 18.5...24.9:
+        categoryLabel.backgroundColor = UIColor.lightGray
+        categoryLabel.text = "Healthy"
+    case 24.9...29.9:
+        categoryLabel.backgroundColor = UIColor.systemYellow
+        categoryLabel.text = "Overweight"
+    case 29.9...34.9:
+        categoryLabel.backgroundColor = UIColor.systemOrange
+        categoryLabel.text = "Severely Overweight"
+    case 35...:
+        categoryLabel.backgroundColor = UIColor.systemRed
+        categoryLabel.text = "Morbidly Overweight"
+    default:
+        break
+        }
+}
+    func formatNumber(value: Float) -> Double{
+        return Double(String(format: "%.2f", value))!
+    }
 
 }
 
